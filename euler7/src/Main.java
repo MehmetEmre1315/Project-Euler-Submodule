d2a743cf2793bb32d8ed44c5a8941768b046e3ae
kahra <kahramanmehmetemre@gmail.com>
2024-04-17 13:12:33 +0300

    euler7 commit

--- euler7/src/Main.java
+++ euler7/src/Main.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+    // firt
     public static void main(String[] args) {
         List<Long> bigList = new ArrayList<>();
         List<Long> primeList = new ArrayList<>();
